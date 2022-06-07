b25974e3731424ff33f4d9ce460c1d463c392e07
Septmiy3 <septimiy64@gmal.com>
2022-06-07 14:59:57 +0800

    Create section products

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-function getPageLinkDomain() {
-    return Array.from(document.getElementsByTagName('a'))
-        .map(link => link.href
-            .replace('http://', '')
-            .replace('https:/', '')
-            .replace('www.', '')
-            .split('/')
-            .shift()
-        )
-        .reduce((unicDomain,domain) => {
-            if (unicDomain.includes(domain)) return unicDomain;
-            return [...unicDomain,domain]
-        },[])
+function burgerMenu() {
+    
 }
